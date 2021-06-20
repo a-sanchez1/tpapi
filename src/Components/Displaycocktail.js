f65ae1ef798fbf1def6efc49e9fce310fc8b00e9
alberto leandro sanchez <beto.unahur@gmail.com>
2021-06-20 17:48:09 -0300

    RETOCADO

--- src/Components/Displaycocktail.js
+++ src/Components/Displaycocktail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Image, Table } from 'react-bootstrap'
 import'./Displaycocktail.css'
 function Displaycocktail({currentCocktail}) {
     if(Object.entries(currentCocktail).length===0){
@@ -14,14 +15,40 @@ function Displaycocktail({currentCocktail}) {
     else{
         return (
             <div className='DisplayCocktail'>
-                <img src={currentCocktail.strDrinkThumb} className='DisplayCocktail__img' />
+                <Image src={currentCocktail.strDrinkThumb} rounded fluid />
+                <Table striped bordered hover variant='dark'>
+                    <tbody>
+                <tr>
+                    <td>DRINK</td>
+                    <td>{currentCocktail.strDrink}</td>    
+                </tr>
+                <tr>
+                    <td>CATEGORY</td>
+                    <td>{currentCocktail.strCategory}</td>    
+                </tr>
+                <tr>
+                    <td>IS ALCOHOLIC?</td>
+                    <td>{currentCocktail.strAlcoholic}</td>    
+                </tr>
+                <tr>
+                    <td>TYPE OF GLASS</td>
+                    <td>{currentCocktail.strGlass}</td>    
+                </tr>
+                <tr>
+                    <td>HOW TO PREPARE</td>
+                    <td>{currentCocktail.strInstructions}</td>    
+                </tr>
+                </tbody>
+                </Table>
+                
+                {/*
                 <div className="DisplayCocktail__info">
                     <p className="name"><span>Drink:</span> {currentCocktail.strDrink}</p>
                     <p className="category"><span>Category:</span> {currentCocktail.strCategory}</p>
                     <p className="alcoholic"><span>Is Alcoholic?</span> {currentCocktail.strAlcoholic} </p>
                     <p className="glass"><span>Type of Glass: </span> {currentCocktail.strGlass}</p>
                     <p className="intructions"><span>How to prepare:</span> {currentCocktail.strInstructions}</p>
-                </div>
+                </div>*/}
             </div>
         )}
 }
